214b8d56a58a814694f5ee495e88dcc79a9fd4eb
Felipe Poletti <felipepolettig@gmail.com>
2023-05-18 18:50:56 -0300

    Create object to-do list and list on screen

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,9 +1,46 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div>Olá React</div>
-  )
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar uma tela de to do list",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+  return <div className="app">
+    <h1>Lista de tarefas</h1>
+    <div className="todo-list">
+      {
+        todos.map((todo) => (
+          <div className="todo">
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+            </div>
+            <div>
+              <button>Done</button>
+              <button>x</button>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  </div>;
 }
 
 export default App
