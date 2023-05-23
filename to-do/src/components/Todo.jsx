eda7fdbbab64cdc945d07ea9aff61c0239ae0298
Felipe Poletti <felipepolettig@gmail.com>
2023-05-22 21:39:16 -0300

    Create complete to do function

--- to-do/src/components/Todo.jsx
+++ to-do/src/components/Todo.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Todo = ({ todo, removeTodo }) => {
+const Todo = ({ todo, completeTodo, removeTodo }) => {
   return (
-    <div className="todo">
+    <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
       <div className="content">
         <p>{todo.text}</p>
         <p className="category">({todo.category})</p>
       </div>
       <div>
-        <button className="complete">Done</button>
+        <button onClick={() => completeTodo(todo.id)} className="complete">Done</button>
         <button onClick={() => removeTodo(todo.id)} className="remove">x</button>
       </div>
     </div>
